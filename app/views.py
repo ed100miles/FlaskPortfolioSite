fb81426b0b9dd88dbda76e918f83fa93f2baddde
ed100miles <edmilestech@gmail.com>
2021-09-14 14:26:44 +0100

    updated to same as server

--- app/views.py
+++ app/views.py
@@ -13,9 +13,13 @@ import json
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        json_body = json.loads(request.data.decode('utf-8')) # parse json
-        possible_words = scrabble.find_words(json_body['board_dict'], json_body['userLetters'])
-        json_response = make_response(jsonify(possible_words), 200)
+        try:
+            json_body = json.loads(request.data.decode('utf-8')) # parse json
+            possible_words = scrabble.find_words(json_body['board_dict'], json_body['userLetters'])
+            json_response = make_response(jsonify(possible_words), 200)
+        except Exception as e:
+            with open('~/FlaskPortfolioSite/log.txt', 'a') as file:
+                file.write(f'e')
         return json_response
     return render_template('index.html')
 
